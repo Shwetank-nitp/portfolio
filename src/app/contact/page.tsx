0812e43ad5feb2debdfec11b9f0e043743b3a7f4
shwetank-nitp <sweetankr.ug23.ee@nitp.ac.in>
2024-08-21 05:11:17 +0530

    init

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,47 +1,41 @@
-"use client";
-
-import { GlareCard } from "@/components/ui/glare-card";
-
 export default function Contact() {
   return (
     <div className="flex h-screen w-screen justify-center items-center bg-gray-900">
-      <div className="mt-11 rounded-lg shadow-lg">
-        <GlareCard className="flex flex-col p-8 justify-center items-center">
-          <div className="text-2xl font-bold text-gray-100 mb-4">
-            Shwetank Rai
-          </div>
-          <div className="text-gray-100">
-            <p>
-              Email:{" "}
-              <a
-                href="mailto:shwetankrai93@gmail.com"
-                className="text-blue-500 hover:underline"
-              >
-                shwetankrai93@gmail.com
-              </a>
-            </p>
-            <p>
-              Phone number:{" "}
-              <a
-                href="tel:+917007792391"
-                className="text-blue-500 hover:underline"
-              >
-                +91 7007792391
-              </a>
-            </p>
-            <p>
-              GitHub:{" "}
-              <a
-                href="https://github.com/Shwetank-nitp/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-500 hover:underline"
-              >
-                https://github.com/Shwetank-nitp/
-              </a>
-            </p>
-          </div>
-        </GlareCard>
+      <div className="bg-slate-800 p-8 rounded-lg shadow-lg">
+        <div className="text-2xl font-bold text-gray-100 mb-4">
+          Shwetank Rai
+        </div>
+        <div className="text-gray-100">
+          <p>
+            Email:{" "}
+            <a
+              href="mailto:shwetankrai93@gmail.com"
+              className="text-blue-500 hover:underline"
+            >
+              shwetankrai93@gmail.com
+            </a>
+          </p>
+          <p>
+            Phone number:{" "}
+            <a
+              href="tel:+917007792391"
+              className="text-blue-500 hover:underline"
+            >
+              +91 7007792391
+            </a>
+          </p>
+          <p>
+            GitHub:{" "}
+            <a
+              href="https://github.com/Shwetank-nitp/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 hover:underline"
+            >
+              https://github.com/Shwetank-nitp/
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   );
